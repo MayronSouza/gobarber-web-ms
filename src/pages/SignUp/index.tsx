c7bb360f1c120d7b4902877b1922c80033ac760f
MayronSouza <mayron.souza@gmz.solutions>
2020-09-11 11:06:13 -0300

    Colocando foco nos campos. E validando os mesmos com o pacote Yup

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -10,9 +11,25 @@ import logoImg from '../../assets/logo.svg';
 import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
-  function handleSubmit(data: object): void {
-    console.log(data);
-  }
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().min(6, 'No mínimo 6 dígitos'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
 
   return (
     <Container>
@@ -24,7 +41,7 @@ const SignUp: React.FC = () => {
           <h1>Faça seu logon</h1>
 
           <Input icon={FiUser} name="name" placeholder="Nome" />
-          <Input icon={FiMail} name="e-mail" placeholder="E-mail" />
+          <Input icon={FiMail} name="email" placeholder="E-mail" />
           <Input
             icon={FiLock}
             name="password"
