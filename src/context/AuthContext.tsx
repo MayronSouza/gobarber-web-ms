50db1cbecfcf69bf6c6aea47184c7234cc4f93bd
MayronSouza <mayron.souza@gmz.solutions>
2020-09-12 20:46:46 -0300

    Criando nosso proprio hook que sera responsavel por retornar os metodos de autenticaçao e as informaçoes salvas no estado do contexto

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,33 +1,63 @@
-import React, { createContext, useCallback } from 'react';
+import React, { createContext, useCallback, useContext, useState } from 'react';
 import api from '../services/api';
 
+interface AuthState {
+  token: string;
+  user: object;
+}
 interface SignInCredentials {
   email: string;
   password: string;
 }
 
 interface AuthContextData {
-  name: string;
+  user: object;
   signIn(credentials: SignInCredentials): Promise<void>;
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<AuthState>(() => {
+    const token = localStorage.getItem('@GoStackMS: token');
+    const user = localStorage.getItem('@GoStackMS: user');
+
+    if (token && user) {
+      return { token, user: JSON.parse(user) };
+    }
+
+    return {} as AuthState;
+  });
+
   const signIn = useCallback(async ({ email, password }) => {
     const { data } = await api.post('sessions', {
       email,
       password,
     });
 
-    console.log(data);
+    const { token, user } = data;
+
+    localStorage.setItem('@GoStackMS: token', token);
+    localStorage.setItem('@GoStackMS: user', JSON.stringify(user));
+
+    setData({ token, user });
   }, []);
 
   return (
-    <AuthContext.Provider value={{ name: 'Mayron Souza', signIn }}>
+    <AuthContext.Provider value={{ user: data.user, signIn }}>
       {children}
     </AuthContext.Provider>
   );
 };
 
-export { AuthContext, AuthProvider };
+function useAuth(): AuthContextData {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+
+  return context;
+}
+
+export { AuthProvider, useAuth };
