120669f04d126e80d8af6d817682e79f40cd40a5
MayronSouza <mayron.souza@gmz.solutions>
2020-11-06 12:41:16 -0300

    Criando a estrutura e estilizaçao para a listagem dos agendamentos da parte da manha e tarde, pensando no funcionamento real da aplicaçao

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FiClock, FiPower } from 'react-icons/fi';
 
 import {
@@ -9,6 +9,8 @@ import {
   Content,
   Schedule,
   NextAppointment,
+  Section,
+  Appointment,
   Calendar,
 } from './styles';
 
@@ -16,6 +18,8 @@ import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
   const { signOut, user } = useAuth();
 
   return (
@@ -62,6 +66,61 @@ const Dashboard: React.FC = () => {
               </span>
             </div>
           </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTuoaC-QBBFgMTXhL8ATmQO7mKT0DLBE_tqaA&usqp=CAU"
+                  alt="Mayron Souza"
+                />
+
+                <strong>Mayron Souza</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong>Tarde</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTuoaC-QBBFgMTXhL8ATmQO7mKT0DLBE_tqaA&usqp=CAU"
+                  alt="Mayron Souza"
+                />
+
+                <strong>Mayron Souza</strong>
+              </div>
+            </Appointment>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTuoaC-QBBFgMTXhL8ATmQO7mKT0DLBE_tqaA&usqp=CAU"
+                  alt="Mayron Souza"
+                />
+
+                <strong>Mayron Souza</strong>
+              </div>
+            </Appointment>
+          </Section>
         </Schedule>
         <Calendar />
       </Content>
