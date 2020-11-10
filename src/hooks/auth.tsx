bff1be8e7d9d816e6b917621a1a6d07974997ffe
MayronSouza <mayron.souza@gmz.solutions>
2020-11-10 16:55:38 -0300

    Implementando o envio de um novo avatar para atualizaçao do perfil do usuario. A atualizaçao do avatar ocorre em uma requisiçao diferente da atualizaçao dos dados cadastrais. Satisfazendo, assim, os requsitos da aplicaçao.

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -21,6 +21,7 @@ interface AuthContextData {
   user: UserAuth;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
+  updateUser(user: UserAuth): void;
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
@@ -62,8 +63,22 @@ const AuthProvider: React.FC = ({ children }) => {
     setData({} as AuthState);
   }, []);
 
+  const updateUser = useCallback(
+    (user: UserAuth) => {
+      localStorage.setItem('@GoStackMS: user', JSON.stringify(user));
+
+      setData({
+        token: data.token,
+        user,
+      });
+    },
+    [data.token],
+  );
+
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+    <AuthContext.Provider
+      value={{ user: data.user, signIn, signOut, updateUser }}
+    >
       {children}
     </AuthContext.Provider>
   );
